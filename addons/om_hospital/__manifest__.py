06f186f82f1cfef6273f9a7e0864738e4befdaa1
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-10-02 09:31:31 +0000

    Create Module In Odoo

---
+++ addons/om_hospital/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Hospital Management",
+    "version": "1.0.0",
+    "author": "Md. Shihab Uddin",
+    "category": "generic module",
+    "description": """
+    Hospital Management
+    """,
+    "depends": [],
+    "data": [], 
+    "demo_xml": [],
+    "installable": True,
+    "application": True,
+    "license": "AGPL-3", 
+}
