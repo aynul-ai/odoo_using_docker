2085cbfef8ad806ddfd9900e4d3281af8a80f66c
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-10-03 16:38:33 +0000

    Add Date And Datetime Fields In Odoo

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -7,3 +7,5 @@ class HospitalAppointment(models.Model):
     _description = "Appointment Record"
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient', required=True)
+    appointment_time = fields.Datetime(string='Appointment Time', required=True, default=fields.Datetime.now)
+    booking_date = fields.Date(string='Booking Date', required=True, default=fields.Date.today)
