38dbef6392d78a7d98c11da6b316775db074d287
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-10-04 06:01:02 +0000

    Add Related Fields In Odoo

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -7,5 +7,6 @@ class HospitalAppointment(models.Model):
     _description = "Appointment Record"
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient', required=True)
+    patient_gender = fields.Selection(related='patient_id.gender', readonly=False, help="non readonly can effect parent model") # non readonly can effect parent model
     appointment_time = fields.Datetime(string='Appointment Time', required=True, default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', required=True, default=fields.Date.today)
