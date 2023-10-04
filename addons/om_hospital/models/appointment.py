0ed206a8c0452bd7375d615476e33bfe3f4fd3d6
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-10-04 08:40:49 +0000

    Define Onchange Functions In Odoo

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -10,3 +10,8 @@ class HospitalAppointment(models.Model):
     patient_gender = fields.Selection(related='patient_id.gender', readonly=False, help="non readonly can effect parent model") # non readonly can effect parent model
     appointment_time = fields.Datetime(string='Appointment Time', required=True, default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', required=True, default=fields.Date.today)
+    ref = fields.Char(string='Reference', required=False)
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
