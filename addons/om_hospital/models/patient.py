9d93f88621cb638b189c9b864f356efc852b363b
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-10-04 06:31:26 +0000

    Create Computed Field In Odoo

--- addons/om_hospital/models/patient.py
+++ addons/om_hospital/models/patient.py
@@ -8,8 +8,18 @@ class HospitalPatient(models.Model):
     
 
     name = fields.Char(string='Name', required=True, tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth', tracking=True)
+    age = fields.Integer(string='Age', tracking=True, compute='_compute_age', help="Computed field(non stored) cannot be used in search view")
     gender = fields.Selection([
         ("male", "Male"), ("female", "Female")])
     ref = fields.Char(string='Reference', required=False)
     active = fields.Boolean(string='Active', default=True)
+
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = fields.Date.today().year - rec.date_of_birth.year
+            else:
+                rec.age = 0
