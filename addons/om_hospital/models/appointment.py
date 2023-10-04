8e1faf49df2afd4da7c3327bc17a8efe8bbf2bdd
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-10-04 08:52:24 +0000

    Rec Name For Models in Odoo

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -5,11 +5,16 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Appointment Record"
+    _rec_name = "patient_id"
 
-    patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient', required=True)
-    patient_gender = fields.Selection(related='patient_id.gender', readonly=False, help="non readonly can effect parent model") # non readonly can effect parent model
-    appointment_time = fields.Datetime(string='Appointment Time', required=True, default=fields.Datetime.now)
-    booking_date = fields.Date(string='Booking Date', required=True, default=fields.Date.today)
+    patient_id = fields.Many2one(
+        comodel_name='hospital.patient', string='Patient', required=True)
+    patient_gender = fields.Selection(related='patient_id.gender', readonly=False,
+                                      help="non readonly can effect parent model")  # non readonly can effect parent model
+    appointment_time = fields.Datetime(
+        string='Appointment Time', required=True, default=fields.Datetime.now)
+    booking_date = fields.Date(
+        string='Booking Date', required=True, default=fields.Date.today)
     ref = fields.Char(string='Reference', required=False)
 
     @api.onchange('patient_id')
