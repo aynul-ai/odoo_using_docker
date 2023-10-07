1b304a0f733a8d06e005e931fe4124f18585d3c4
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-10-07 14:50:00 +0000

    How To Override Create Method In Odoo

--- addons/om_hospital/models/patient.py
+++ addons/om_hospital/models/patient.py
@@ -5,11 +5,11 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Patient Record'
-    
 
     name = fields.Char(string='Name', required=True, tracking=True)
     date_of_birth = fields.Date(string='Date of Birth', tracking=True)
-    age = fields.Integer(string='Age', tracking=True, compute='_compute_age', help="Computed field(non stored) cannot be used in search view")
+    age = fields.Integer(string='Age', tracking=True, compute='_compute_age',
+                         help="Computed field(non stored) cannot be used in search view")
     gender = fields.Selection([
         ("male", "Male"), ("female", "Female")])
     ref = fields.Char(string='Reference', required=False)
@@ -17,11 +17,17 @@ class HospitalPatient(models.Model):
     image = fields.Binary(string='Image')
     tag_ids = fields.Many2many('patient.tag', string='Tags')
 
-
     @api.depends('date_of_birth')
     def _compute_age(self):
         for rec in self:
             if rec.date_of_birth:
                 rec.age = fields.Date.today().year - rec.date_of_birth.year
             else:
-                rec.age = 0 # else is mendatory from odoo 14
+                rec.age = 0  # else is mendatory from odoo 14
+
+    @api.model
+    def create(self, vals):
+        if vals['ref'] is False:
+            vals['ref'] = "testing124"
+        res = super(HospitalPatient, self).create(vals)
+        return res
