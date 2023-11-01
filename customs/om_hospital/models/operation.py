0fd0eda82b8e056e8cb2b02f7fc072076ff6ab36
Md. Shihab Uddin <pythonpiash@gmail.com>
2023-11-01 14:52:52 +0600

    Reference Field In Odoo || Odoo Reference Field

--- customs/om_hospital/models/operation.py
+++ customs/om_hospital/models/operation.py
@@ -8,13 +8,11 @@ class HospitalOperation(models.Model):
 
     doctor_id = fields.Many2one('res.users', string="Doctor")
     operation_name = fields.Char(string="Operation Name")
-
+    reference_record = fields.Reference(selection=[('hospital.patient', 'Patient'), ('hospital.appointment', 'Appointment')],
+                                        string="Record")
 
     @api.model
     def name_create(self, name):
         return self.create({
             'operation_name': name
         }).name_get()[0]
-
-
-
